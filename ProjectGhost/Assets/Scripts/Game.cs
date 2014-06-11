3fc0a49a6d3c0abb7816c9b6130e0fc62c696e0b
Evgeniy Medvedev <jakedesu@gmail.com>
2014-06-12 01:54:03 +0400

    Tracking stuff

---
+++ ProjectGhost/Assets/Scripts/Game.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Game {
+
+	public enum State {Running, Paused};
+	// Use this for initialization
+	public static State CurrentState;
+
+	private static float timeScaleStored = 1;
+
+	public static void Pause()
+	{
+		CurrentState = State.Paused;
+
+		timeScaleStored = Time.timeScale;
+		Time.timeScale = 0;
+	}
+
+	public static void Unpause()
+	{
+		CurrentState = State.Running;
+
+		Time.timeScale = timeScaleStored;
+	}
+}
