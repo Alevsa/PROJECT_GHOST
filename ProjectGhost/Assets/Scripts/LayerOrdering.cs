42fc2c9bb93f153f5cde05467cf2514acab41742
Evgeniy Medvedev <jakedesu@gmail.com>
2014-07-03 15:49:07 +0400

    Added layer ordering

---
+++ ProjectGhost/Assets/Scripts/LayerOrdering.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerOrdering : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+	private float LowestPoint;
+	private Transform hui;
+	private int sortingorder;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sprite = GetComponent<SpriteRenderer>();
+		LowestPoint = (sprite.transform.position.y - sprite.bounds.size.y / 2);
+		sortingorder = Mathf.FloorToInt(10000 / LowestPoint);
+		GetComponent<SpriteRenderer>().sortingOrder = sortingorder;
+		Debug.Log(LowestPoint);
+	}
+}
