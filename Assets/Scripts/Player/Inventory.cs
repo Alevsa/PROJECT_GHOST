768e9f9a86b4b201763b168130ef015d839ed4c9
Evgeniy <jakedesu@gmail.com>
2014-08-15 17:59:50 +0400

    Added inventory system

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	public List<Item> items;
+
+	// Use this for initialization
+	void Start () {
+		items = new List<Item> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Item i in items)
+		{
+			Debug.Log (i.Name);
+		}
+	}
+
+	public void Get (Item item) {
+		if (item.MaxStackSize > 1)
+		{
+			foreach (Item i in items)
+			{
+				if ((i.id == item.id) && (i.StackSize + item.StackSize <= i.MaxStackSize))
+				{
+					i.StackSize += item.StackSize;
+					break;
+				}
+			}
+		}
+		else 
+			items.Add (item);
+	}
+
+	public void Lose (Item item) {
+		foreach (Item i in items) 
+		{
+			if (i.id == item.id)
+			{
+				if (i.StackSize < item.StackSize)
+				{
+					i.StackSize -= item.StackSize;
+				}
+				else 
+					items.Remove(i);
+
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Sword.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+	Weapon sword;
+	// Use this for initialization
+	void Start () {
+		sword = new Weapon ();
+		sword.id = 1;
+		sword.Name = "Sword";
+		sword.Category = "Weapon";
+		sword.Damage = 1;
+		sword.MaxStackSize = 1;
+		sword.StackSize = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find ("PlayerMeta").GetComponent<Inventory> ().Get (sword);
+		Destroy (this.gameObject);
+	}
+}
